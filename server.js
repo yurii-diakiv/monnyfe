f98c78459f86e970f3d25ab9d066ca418f1227ba
Yurii Diakiv <yuradyakiv@gmail.com>
2020-05-04 00:59:09 +0300

    update settings5

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname+'/dist/MonnyFe'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/MonnyFE/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 
 app.listen(process.env.PORT || 8080);
